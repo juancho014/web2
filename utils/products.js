1b44487b71b7390e27bda4dca54fcaf93a7eac4b
juanfb19@gmail.com <juanfb19@gmail.com>
2021-08-23 21:35:49 -0300

    web

--- utils/products.js
+++ utils/products.js
@@ -1,7 +1,7 @@
 const request = require('postman-request');
 
 const getAllJuegos = (callback) => {
-    request('http://localhost:3001/juegos', (error, res, body) => {
+    request('https://juegos-consolas-web.herokuapp.com/juegos', (error, res, body) => {
         if(error){
           console.log('Error', error)  
           return callback(error, undefined);
